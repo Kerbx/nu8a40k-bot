f72e0bca4d2c8f4a855670a51756f1e2fcd13799
Kerbx <r3ostat@ya.ru>
2024-01-29 18:03:12 +0300

    added commands user and info to see info about user.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import config
 import database
+import globals
 
 import telebot.async_telebot
 
 
 bot = telebot.async_telebot.AsyncTeleBot(config.BOT_TOKEN)
+database.db.connect()
 
 
 @bot.message_handler(commands=['start'])
@@ -15,8 +17,8 @@ async def send_start(message):
 @bot.message_handler(commands=['register'])
 async def register_user(message):
     for user in database.User.select():
-        if user.id == message.from_user.id:
-            return
+        if user.id == str(message.from_user.id):
+            return            
     
     admins = await bot.get_chat_administrators(message.chat.id)
     
@@ -34,11 +36,32 @@ async def register_user(message):
     USER_NAME = message.from_user.first_name
     USER_WARNS = 0
     USER_STATE = 0
+    try:
+        user = database.User.create(id=str(USER_ID),
+                            name=str(USER_NAME),
+                            rank=USER_RANK,
+                            admin=USER_IS_ADMIN,
+                            exp=USER_EXP,
+                            warns=USER_WARNS,
+                            state=USER_STATE)
+    except Exception as e:
+        print(e)
     
-    user = database.User(id=USER_ID, name=USER_NAME, rank=USER_RANK, admin=USER_IS_ADMIN, exp=USER_EXP, warns=USER_WARNS, state=USER_STATE)
-    user.save()
     
-    
+@bot.message_handler(commands=['user', 'info'])
+async def get_user_info(message):
+    for user in database.User.select():
+        if message.reply_to_message:
+            message.from_user.id = message.reply_to_message.from_user.id
+        if user.id == str(message.from_user.id):
+            info = f"""
+            Имя: {user.name}
+            Статус: {globals.USER_STATES.get(user.state)}
+            Ранг: {globals.USER_RANKS.get(user.rank)}
+            """
+            await bot.send_message(message.chat.id, f'Информация о пользователе:\n{info}')
+
+
 @bot.message_handler(func=lambda message: True)
 async def send_any(message):
     print(message)
